c73ea2a1ebdcd638f5197bf717cc0f9895cd2783
Sanjid Rahman <rahmansanjid505@gmail.com>
2024-03-19 14:13:38 +0530

    budget changes

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css']
 })
-export class CheckoutComponent {
+export class CheckoutComponent implements OnInit, OnDestroy {
+
+  ngOnInit(): void {
+    
+  }
+
+  ngOnDestroy(): void {
+    
+  }
 
 }
