8b6bec0ca4b25044f532854f446cac1e2d26c22d
Sanjid Rahman <rahmansanjid505@gmail.com>
2024-03-19 14:57:30 +0530

    url changes (AWS)

--- src/environments/environment.developement.ts
+++ src/environments/environment.developement.ts
@@ -1,4 +1,5 @@
 export const environment = {
     // API_URL: 'http://localhost:3030',
-    API_URL: 'https://mt-backend-b58v.onrender.com'
+    // API_URL: 'https://mt-backend-b58v.onrender.com',
+    API_URL: 'https://wheelsondemand.online'
 }
